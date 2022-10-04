65d59b7d5c7d22e18725582a207f7732129c58dd
KangChangHyeok <dpkyle97@gmail.com>
2022-10-04 17:30:12 +0900

    Initial Commit

---
+++ TwitterPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterPractice
+//
+//  Created by 강창혁 on 2022/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
